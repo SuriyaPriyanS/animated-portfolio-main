e99a66e5f359eaae9d7f8e80c8a9912fd46ceca9
Suriya Priyan <suriyapriyan0506@gmail.com>
2025-08-09 10:08:17 +0530

    updated age

--- utils/mySelf.js
+++ utils/mySelf.js
@@ -1,7 +1,7 @@
 const mySelf = {
     heading: 'My About',
     paragraphs: [
-       `Hello! My name is Suriyapriyan, and I’m a passionate Full Stack Developer currently working at MercuryMinds Private Limited. With 1 year of experience, I've had the opportunity to work on three diverse projects across CMS, CRM, and Healthcare domains. My journey into programming began at a young age — I was fascinated by how things worked behind the screen. I spent hours playing with the Inspect Element tool, tweaking colors, writing fun loops, and even experimenting with screen lock bypasses. At just 16, I made the decision to turn this passion into a career`
+       `Hello! My name is Suriyapriyan, and I’m a passionate Full Stack Developer currently working at MercuryMinds Private Limited. With 1 year of experience, I've had the opportunity to work on three diverse projects across CMS, CRM, and Healthcare domains. My journey into programming began at a young age — I was fascinated by how things worked behind the screen. I spent hours playing with the Inspect Element tool, tweaking colors, writing fun loops, and even experimenting with screen lock bypasses. At just 27, I made the decision to turn this passion into a career`
     ],
 
 
